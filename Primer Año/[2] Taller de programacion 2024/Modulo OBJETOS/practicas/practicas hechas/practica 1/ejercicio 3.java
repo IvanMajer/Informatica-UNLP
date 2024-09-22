68f12722c67107e51ecae6f8a9daff5e76dec354
IvanMajer <ivan.majeer@gmail.com>
2024-09-22 02:00:48 -0300

    Add files via upload

---
+++ Primer Año/[2] Taller de programacion 2024/Modulo OBJETOS/practicas/practicas hechas/practica 1/ejercicio 3.java
@@ -0,0 +1,63 @@
+package tema1;
+
+import PaqueteLectura.GeneradorAleatorio;
+
+public class Ej03Matrices {
+    
+    
+    public static void main(String[] args) {
+	GeneradorAleatorio.iniciar();
+	
+        int cantFilas = 5;
+        int cantColumnas = 5; 
+        int [][] matriz; 
+        int dimF = 5;
+        matriz = new int [cantFilas][cantColumnas];
+        int total;
+        int [] vector;
+        vector = new int [dimF];
+        total = 0 ;
+        for (int f=0; f <cantFilas; f++)
+        {
+            for (int c=0;c<cantColumnas;c++)
+            {
+                matriz[f][c] = GeneradorAleatorio.generarInt(31);     
+            }
+        }
+       
+    
+        for (int f= 0; f <cantFilas;f++ )
+        {
+            for (int c=0;c<cantColumnas; c++)
+            {
+                System.out.print ("["+f+c+"] "+ matriz[f][c] +  " |");
+            }
+            System.out.println ("");
+        }
+
+        for (int c=0;c<cantColumnas;c++)
+        {
+            int f=0;
+            total = total + matriz[f][c];
+        }
+       System.out.println ("La suma de los elementos de la fila 1 es: " + total);
+       
+       
+        for (int i=0; i<5; i++)
+        {
+            vector[i] = 0; 
+        }
+        for (int c=0;c<cantColumnas;c++) 
+        {
+            for (int f=0;f<cantFilas;f++)
+            {
+                vector[c] = vector[c]+ matriz[f][c];
+            }
+        }
+        
+        for (int n=0; n<=4; n++)
+            System.out.println (vector[n]);
+        
+    
+    }
+}
